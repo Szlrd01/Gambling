39196dd8ec866970c065986740f759ee7e4db99e
Szlrd01 <szilardalegjobb@outlook.hu>
2026-04-13 13:45:08 +0200

    #2
    Mentés: Hibajavítások, pénz funkció berakva

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import random
 import os
-def fej_iras():
+import tkinter as tk
+
+penz = 1000
+
+   
+def fej_iras(penz):
+    penz=int(penz)
+    os.system('cls' if os.name == 'nt' else 'clear')
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f'Egyenleged: {penz}')
+        tet = int(input('Add meg a tétet: '))
+        penz-=tet
         print("Fej vagy írás játék!")
         print("Írj be: fej vagy írás | 0 - kilépés")
         valasztas = input("A választásod: ").lower()
@@ -10,22 +19,64 @@ def fej_iras():
         print(f"A gép dobása: {gep}")
 
         if valasztas == gep:
-            print("Eltaláltad! ")
+                    penz+=tet*2
+                    print("Eltaláltad! ")
+                    
         elif valasztas in ["fej", "írás"]:
-            print("Nem találtad el ")
+                    penz-=tet
+                    print("Nem találtad el ")
+
         elif valasztas == '0':
             os.system('cls' if os.name == 'nt' else 'clear')
             break
         else:
             print("Érvénytelen választás!")
-            
-    
+    with open('penz.txt', 'w+',encoding='utf-8') as f:
+        f.write(str(penz))
+def blackjack():
+    while True:
+        penz = input('Rakd fel tétet: ')
+        if int(penz) > 0:
+            x = random.randint(1,10)
+            y = random.randint(1,10)
+            print(f'A kártyáid: {x} és {y}')  
+            print(f'🂼 {y}, {x}')
+        elif int(penz) == 0:
+            break
+def vesztes():
+    root = tk.Tk()
+    root.attributes("-topmost", True) 
+    label = tk.Label(root, text="Vesztes vagy", font=("Arial", 190))
+    label.pack()
+    root.mainloop()
     
 
+    
+
+
 while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
-    x = int(input(f'Mit akarsz játszani?\n(1) Fej vagy írás\n(2)\n(3)\nVálasztásod: '))
-    if x == 1:
-        fej_iras()
-    elif x == 0:
-        break
+    with open('penz.txt', 'r',encoding='utf-8') as f:
+        penz = f.read()
+    penz=int(penz)
+    if penz <= 0:
+        vesztes()
+        
+        
+        
+        
+    
+    else:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print('----  Akóts Bálint Kaszinó  ----')
+        print(f'Egyenleged: {penz}')
+        x = int(input(f'Mit akarsz játszani?\n(1) Fej vagy írás\n(2) BlackJack\n(3)\n(0) Kilépés\nVálasztásod: '))  
+        if x == 1:
+            fej_iras(penz)
+        elif x == 2:
+            blackjack()
+        elif x == 3:
+            penz=0
+        elif x == 0:
+            break
+        
+
---
+++ teszt.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+label = tk.Label(root, text="VESZTES VAGY", font=("Arial", 190))
+label.pack()
+
+root.mainloop()
+
+""" 
+<font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">🂽</font></font>
+
+
+"""
